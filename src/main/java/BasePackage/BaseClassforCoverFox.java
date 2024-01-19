dcd4d46510ca97773efd3016337d4a19f4aa286c
SonaliJawajiwar <sonali.jawajiwar@gmail.com>
2024-01-19 22:54:39 +0530

    [Sonali]:adding pu

--- src/main/java/BasePackage/BaseClassforCoverFox.java
+++ src/main/java/BasePackage/BaseClassforCoverFox.java
@@ -2,18 +2,17 @@ package BasePackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Reporter;
 
 import UtilityPackage.UtilityClassCommonMethodsUse;
 
 public class BaseClassforCoverFox {
 	protected static WebDriver driver;
-ChromeOptions opt;
-	// open browser
-	public void launchcoverFoxBrowser() {
-       opt=new ChromeOptions();
-       opt.addArguments("--disable-notifications");
+//     ChromeOptions opt;
+//	// open browser
+      public  void launchcoverFoxBrowser() {
+//       opt=new ChromeOptions();
+//       opt.addArguments("--disable-notifications");
 		driver = new ChromeDriver();
 		Reporter.log("Launching Browser", true);
 		driver.get("https://www.coverfox.com/");
@@ -23,7 +22,7 @@ ChromeOptions opt;
 	}
 
 	// close browser
-	public void closeBrowser1() throws InterruptedException {
+	public  void closeBrowser1() throws InterruptedException {
 		driver.close();
 		Thread.sleep(1000);
 		Reporter.log("closing Browser", true);
